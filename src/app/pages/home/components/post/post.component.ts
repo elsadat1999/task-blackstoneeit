5c3c587bea2f5173196ed20b4bbca4aa7463150a
blaktiger205 <blaktiger205@gmail.com>
2022-07-19 02:02:37 +0200

    add

--- src/app/pages/home/components/post/post.component.ts
+++ src/app/pages/home/components/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-
+  @Input() image:string = '';
+  @Input() avatar:string = '';
   constructor() { }
 
   ngOnInit(): void {
